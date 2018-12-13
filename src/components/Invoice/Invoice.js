770d742761676351489e0346d2c82003809eeb29
Nicholas Callias <nnccnch@gmail.com>
2018-12-13 14:54:11 +0200

    Invoice changed

--- src/components/Invoice/Invoice.js
+++ src/components/Invoice/Invoice.js
@@ -1,181 +1,190 @@
-import React, { Component } from 'react'
+import * as React from 'react'
 import styles from './Invoice.module.scss'
 
 import LineItems from './LineItems'
 
 import uuidv4 from 'uuid/v4'
 
-class Invoice extends Component {
+const { useState } = React
 
-  locale = 'en-US'
-  currency = 'USD'
+const Invoice = () => {
+  const locale = 'en-ZA'
+  const currency = 'ZAR'
 
-  state = {
-    taxRate: 0.00,
+  const [state, setState] = useState({
+    taxRate: 14.0,
     lineItems: [
       {
-        id: 'initial',      // react-beautiful-dnd unique key
+        id: 'initial', // react-beautiful-dnd unique key
         name: '',
         description: '',
         quantity: 0,
-        price: 0.00,
-      },
+        price: 0.0
+      }
     ]
-  }
+  })
 
-  handleInvoiceChange = (event) => {
-    this.setState({[event.target.name]: event.target.value})
-  }
+  const handleInvoiceChange = event =>
+    setState({ [event.target.name]: event.target.value })
 
-  handleLineItemChange = (elementIndex) => (event) => {
-    let lineItems = this.state.lineItems.map((item, i) => {
+  const handleLineItemChange = elementIndex => event => {
+    let lineItems = state.lineItems.map((item, i) => {
       if (elementIndex !== i) return item
-      return {...item, [event.target.name]: event.target.value}
+      return { ...item, [event.target.name]: event.target.value }
     })
-    this.setState({lineItems})
+    setState({ lineItems })
   }
 
-  handleAddLineItem = (event) => {
-    this.setState({
+  const handleAddLineItem = event =>
+    setState({
       // use optimistic uuid for drag drop; in a production app this could be a database id
-      lineItems: this.state.lineItems.concat(
-        [{ id: uuidv4(), name: '', description: '', quantity: 0, price: 0.00 }]
-      )
+      lineItems: state.lineItems.concat([
+        { id: uuidv4(), name: '', description: '', quantity: 0, price: 0.0 }
+      ])
     })
-  }
 
-  handleRemoveLineItem = (elementIndex) => (event) => {
-    this.setState({
-      lineItems: this.state.lineItems.filter((item, i) => {
+  const handleRemoveLineItem = elementIndex => event =>
+    setState({
+      lineItems: state.lineItems.filter((item, i) => {
         return elementIndex !== i
       })
     })
-  }
 
-  handleReorderLineItems = (newLineItems) => {
-    this.setState({
-      lineItems: newLineItems,
+  const handleReorderLineItems = newLineItems =>
+    setState({
+      lineItems: newLineItems
     })
-  }
 
-  handleFocusSelect = (event) => {
-    event.target.select()
-  }
+  const handleFocusSelect = event => event.target.select()
 
-  formatCurrency = (amount) => {
-    return (new Intl.NumberFormat(this.locale, {
+  const formatCurrency = amount =>
+    new Intl.NumberFormat(locale, {
       style: 'currency',
-      currency: this.currency,
+      currency: currency,
       minimumFractionDigits: 2,
       maximumFractionDigits: 2
-    }).format(amount))
-  }
+    }).format(amount)
 
-  calcTaxAmount = (c) => {
-    return c * (this.state.taxRate / 100)
-  }
+  const calcTaxAmount = c => c * (state.taxRate / 100)
 
-  calcLineItemsTotal = () => {
-    return this.state.lineItems.reduce((prev, cur) => (prev + (cur.quantity * cur.price)), 0)
-  }
+  const calcLineItemsTotal = () =>
+    state.lineItems.reduce((prev, cur) => prev + cur.quantity * cur.price, 0)
 
-  calcTaxTotal = () => {
-    return this.calcLineItemsTotal() * (this.state.taxRate / 100)
-  }
+  const calcTaxTotal = () => calcLineItemsTotal() * (state.taxRate / 100)
 
-  calcGrandTotal = () => {
-    return this.calcLineItemsTotal() + this.calcTaxTotal()
-  }
+  const calcGrandTotal = () => calcLineItemsTotal() + calcTaxTotal()
 
-  render = () => {
-    return (
-
-      <div className={styles.invoice}>
-        <div className={styles.brand}>
-          <img src="https://via.placeholder.com/150x50.png?text=logo" alt="Logo" className={styles.logo} />
+  // const render = () => {
+  return (
+    <div className={styles.invoice}>
+      <div className={styles.brand}>
+        <img
+          src='https://via.placeholder.com/150x50.png?text=logo'
+          alt='Logo'
+          className={styles.logo}
+        />
+      </div>
+      <div className={styles.addresses}>
+        <div className={styles.from}>
+          <strong>Amazing Company</strong>
+          <br />
+          123 Kensington Ave
+          <br />
+          Toronto, ON, Canada &nbsp;A1B2C3
+          <br />
+          416-555-1234
         </div>
-        <div className={styles.addresses}>
-          <div className={styles.from}>
-            <strong>Amazing Company</strong><br />
-              123 Kensington Ave<br />
-              Toronto, ON, Canada &nbsp;A1B2C3<br />
-              416-555-1234
-          </div>
-          <div>
-            <div className={`${styles.valueTable} ${styles.to}`}>
-              <div className={styles.row}>
-                <div className={styles.label}>Customer #</div>
-                <div className={styles.value}>123456</div>
-              </div>
-              <div className={styles.row}>
-                <div className={styles.label}>Invoice #</div>
-                <div className={styles.value}>123456</div>
-              </div>
-              <div className={styles.row}>
-                <div className={styles.label}>Date</div>
-                <div className={`${styles.value} ${styles.date}`}>2019-01-01</div>
-              </div>
+        <div>
+          <div className={`${styles.valueTable} ${styles.to}`}>
+            <div className={styles.row}>
+              <div className={styles.label}>Customer #</div>
+              <div className={styles.value}>123456</div>
+            </div>
+            <div className={styles.row}>
+              <div className={styles.label}>Invoice #</div>
+              <div className={styles.value}>123456</div>
+            </div>
+            <div className={styles.row}>
+              <div className={styles.label}>Date</div>
+              <div className={`${styles.value} ${styles.date}`}>2019-01-01</div>
             </div>
           </div>
         </div>
-        <h2>Invoice</h2>
+      </div>
+      <h2>Invoice</h2>
 
-          <LineItems
-            items={this.state.lineItems}
-            currencyFormatter={this.formatCurrency}
-            addHandler={this.handleAddLineItem}
-            changeHandler={this.handleLineItemChange}
-            focusHandler={this.handleFocusSelect}
-            deleteHandler={this.handleRemoveLineItem}
-            reorderHandler={this.handleReorderLineItems}
-          />
+      <LineItems
+        items={state.lineItems}
+        currencyFormatter={formatCurrency}
+        addHandler={handleAddLineItem}
+        changeHandler={handleLineItemChange}
+        focusHandler={handleFocusSelect}
+        deleteHandler={handleRemoveLineItem}
+        reorderHandler={handleReorderLineItems}
+      />
 
-        <div className={styles.totalContainer}>
-          <form>
-            <div className={styles.valueTable}>
-              <div className={styles.row}>
-                <div className={styles.label}>Tax Rate (%)</div>
-                <div className={styles.value}><input name="taxRate" type="number" step="0.01" value={this.state.taxRate} onChange={this.handleInvoiceChange} onFocus={this.handleFocusSelect} /></div>
+      <div className={styles.totalContainer}>
+        <form>
+          <div className={styles.valueTable}>
+            <div className={styles.row}>
+              <div className={styles.label}>Tax Rate (%)</div>
+              <div className={styles.value}>
+                <input
+                  name='taxRate'
+                  type='number'
+                  step='0.01'
+                  value={state.taxRate}
+                  onChange={handleInvoiceChange}
+                  onFocus={handleFocusSelect}
+                />
               </div>
             </div>
-          </form>
-          <form>
-            <div className={styles.valueTable}>
-              <div className={styles.row}>
-                <div className={styles.label}>Subtotal</div>
-                <div className={`${styles.value} ${styles.currency}`}>{this.formatCurrency(this.calcLineItemsTotal())}</div>
-              </div>
-              <div className={styles.row}>
-                <div className={styles.label}>Tax ({this.state.taxRate}%)</div>
-                <div className={`${styles.value} ${styles.currency}`}>{this.formatCurrency(this.calcTaxTotal())}</div>
-              </div>
-              <div className={styles.row}>
-                <div className={styles.label}>Total Due</div>
-                <div className={`${styles.value} ${styles.currency}`}>{this.formatCurrency(this.calcGrandTotal())}</div>
+          </div>
+        </form>
+        <form>
+          <div className={styles.valueTable}>
+            <div className={styles.row}>
+              <div className={styles.label}>Subtotal</div>
+              <div className={`${styles.value} ${styles.currency}`}>
+                {formatCurrency(calcLineItemsTotal())}
               </div>
             </div>
-          </form>
-        </div>
-
-        <div className={styles.pay}>
-          <button className={styles.payNow}>Pay Now</button>
-        </div>
-
-        <div className={styles.footer}>
-          <div className={styles.comments}>
-            <h4>Notes</h4>
-            <div>By Kevin Firko, consulting developer at <a href="https://bitcurve.com">Bitcurve Systems</a>. Check out my blog: <a href="https://firxworx.com">https://firxworx.com</a>.</div>
+            <div className={styles.row}>
+              <div className={styles.label}>Tax ({state.taxRate}%)</div>
+              <div className={`${styles.value} ${styles.currency}`}>
+                {formatCurrency(calcTaxTotal())}
+              </div>
+            </div>
+            <div className={styles.row}>
+              <div className={styles.label}>Total Due</div>
+              <div className={`${styles.value} ${styles.currency}`}>
+                {formatCurrency(calcGrandTotal())}
+              </div>
+            </div>
           </div>
-          <div className={styles.closing}>
-            <div>Thank-you for your business</div>
-          </div>
-        </div>
-
+        </form>
       </div>
 
-    )
-  }
+      <div className={styles.pay}>
+        <button className={styles.payNow}>Pay Now</button>
+      </div>
 
+      <div className={styles.footer}>
+        <div className={styles.comments}>
+          <h4>Notes</h4>
+          <div>
+            By Kevin Firko, consulting developer at{' '}
+            <a href='https://bitcurve.com'>Bitcurve Systems</a>. Check out my
+            blog: <a href='https://firxworx.com'>https://firxworx.com</a>.
+          </div>
+        </div>
+        <div className={styles.closing}>
+          <div>Thank-you for your business</div>
+        </div>
+      </div>
+    </div>
+  )
 }
+// }
 
 export default Invoice
